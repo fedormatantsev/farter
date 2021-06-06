b6ecc8c09e9b45f91ea94be4c1a8fd3a807962c6
fedormatantsev <fedormatantsev@gmail.com>
2021-06-06 20:53:04 +0300

    wip

--- ml/src/config.py
+++ ml/src/config.py
@@ -4,6 +4,7 @@ import hashlib
 import typing
 import yaml
 import enum
+import os
 
 
 class CandleResolution(enum.Enum):
@@ -38,17 +39,20 @@ class ModelConfig:
     raw_input: typing.List[RawInputConfig]
 
     @staticmethod
-    def from_file(config_name):
-        with open(config_name, 'r') as config_file:
-            config_yaml = yaml.load(config_file, Loader=yaml.CFullLoader)
+    def from_dict(config_dict):
+        training_data = config_dict['training_data']
+        x_definition = config_dict['x_definition']
+        y_definition = config_dict['y_definition']
 
-        model_resolution = CandleResolution[config_yaml['training_data']['resolution']]
-        first_date = datetime.date.fromisoformat(config_yaml['training_data']['first_date'])
-        last_date = datetime.date.fromisoformat(config_yaml['training_data']['last_date'])
+        model_resolution = CandleResolution[training_data['resolution']]
+        first_date = datetime.date.fromisoformat(training_data['first_date'])
+        last_date = datetime.date.fromisoformat(training_data['last_date'])
 
         tickers = set()
-        for x in config_yaml['x_definition']:
+        for x in x_definition:
             tickers.add(x['ticker'])
+
+        tickers.add(y_definition['ticker'])
 
         raw_input = []
         for ticker in tickers:
@@ -57,3 +61,17 @@ class ModelConfig:
             raw_input.append(raw_input_config)
 
         return ModelConfig(raw_input=raw_input)
+
+
+class ConfigLoader:
+    def __init__(self):
+        script_path = os.path.abspath(os.path.dirname(__file__))
+        config_dir = os.path.join(script_path, '..', 'data', 'config')
+        self.config_dir = os.path.normpath(config_dir)
+
+    def load_config(self, config_name: str) -> ModelConfig:
+        config_path = os.path.join(self.config_dir, f'{config_name}.yaml')
+
+        with open(config_path, 'r') as config_file:
+            config_dict = yaml.load(config_file, Loader=yaml.CFullLoader)
+            return ModelConfig.from_dict(config_dict)
